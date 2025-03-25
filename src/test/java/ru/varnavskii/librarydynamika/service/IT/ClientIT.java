89e50a93b8a4d5b25c30c1f917e0be0edfb8b8e1
Igr1x <varnavskii.igrix@mail.ru>
2025-03-25 20:24:37 +0300

    tests

--- src/test/java/ru/varnavskii/librarydynamika/service/IT/ClientIT.java
+++ src/test/java/ru/varnavskii/librarydynamika/service/IT/ClientIT.java
@@ -2,22 +2,22 @@ package ru.varnavskii.librarydynamika.service.IT;
 
 import lombok.SneakyThrows;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import org.springframework.test.web.servlet.MvcResult;
+
 import ru.varnavskii.librarydynamika.controller.dto.ClientIn;
-import ru.varnavskii.librarydynamika.controller.dto.ClientOut;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ClientIT extends IntegrationTestBase {
@@ -32,41 +32,23 @@ public class ClientIT extends IntegrationTestBase {
         LocalDate clientBirthDate = LocalDate.now();
         ClientIn clientIn = new ClientIn(clientFirstName, clientLastName, null, clientBirthDate);
 
-        String response = mockMvc.perform(post(URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(clientIn)))
+        MvcResult result = mockMvc.perform(post("/client")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("firstName", clientFirstName)
+                .param("lastName", clientLastName)
+                .param("birthDate", clientBirthDate.toString()))
+            .andExpect(status().is3xxRedirection())
+            .andReturn();
+        String location = result.getResponse().getHeader("Location");
+        long clientId = Long.parseLong(location.replaceAll("\\/client\\/", ""));
+
+        mockMvc.perform(get("/client/" + clientId))
             .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-        ClientOut client = objectMapper.readValue(response, ClientOut.class);
-
-        Assertions.assertNotNull(client);
-        assertEquals(clientFirstName, client.getFirstName());
-        assertEquals(clientLastName, client.getLastName());
-        Assertions.assertNull(client.getPatronymic());
-        assertEquals(clientBirthDate, client.getBirthDate());
-
-        String newClientPatronymic = "patronymic";
-        clientIn = new ClientIn(clientFirstName, clientLastName, newClientPatronymic, clientBirthDate);
-        response = mockMvc.perform(put(URL + "/" + client.getId())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(clientIn)))
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-        client = objectMapper.readValue(response, ClientOut.class);
-
-        assertEquals(newClientPatronymic, client.getPatronymic());
-
-        response = mockMvc.perform(delete(URL + "/" + client.getId())
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-        assertTrue(response.contains(String.format("Client with id %d deleted", client.getId())));
+            .andExpect(model().attributeExists("client"))
+            .andExpect(model().attribute("client", hasProperty("id", is(clientId))))
+            .andExpect(model().attribute("client", hasProperty("firstName", is(clientFirstName))))
+            .andExpect(model().attribute("client", hasProperty("lastName", is(clientLastName))))
+            .andExpect(model().attribute("client", hasProperty("birthDate", is(clientBirthDate))));
     }
 
     @Test
