88e62dfd9a33b426054fdac8f723592e5bdccb32
Igr1x <varnavskii.igrix@mail.ru>
2025-03-25 15:05:42 +0300

    First commit

---
+++ src/test/java/ru/varnavskii/librarydynamica/LibraryDynamicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.varnavskii.librarydynamica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDynamicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
